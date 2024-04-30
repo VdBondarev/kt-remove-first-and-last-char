94a59c246e4a0af33e214a589b37a2be59dd8fca
Vlad Bondarev <bondarev.21@icloud.com>
2024-04-30 10:38:23 +0300

    done hw

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,9 @@
 package mate.academy
+const val ONE = 1
 
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    if (str != null && str.isNotEmpty()) {
+        return str.substring(ONE, str.length - ONE)
+    }
+    return "";
 }
